a676065f68defd0ba72c1766b5ac92c2dfa7efdc
cami670v <cami670v@ZBC-AA-UBUY12>
2020-08-03 13:42:52 +0200

    Add project files.

---
+++ Rumfanget/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Rumfanget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv Højden her:");
+            double h = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Skriv længden her");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Skriv bredten her");
+            double l = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Rumfanget er " + (h * l * b));
+        }
+    }
+}
